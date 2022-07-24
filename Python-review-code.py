f04ac87e3a805e258e15eafa0cb95b756a4cdfe5
shacharbi23-meet <109904744+shacharbi23-meet@users.noreply.github.com>
2022-07-24 17:08:13 +0300

    Create Python-review-code.py
    Not done yet!

---
+++ Python-review-code.py
@@ -0,0 +1,3 @@
+def create_youtube_video(title, description):
+	youtube_vid = {"Title":title, "Description": description, "Likes": 0, "Dislikes": 0, "Comments": {"username": ""}}
+	return youtube_vid
