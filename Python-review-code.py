c2294ca3b3f82240d3a5102b447e0256cc5670c9
shacharbi23-meet <109904744+shacharbi23-meet@users.noreply.github.com>
2022-07-25 10:47:22 +0300

    Update Python-review-code.py

--- Python-review-code.py
+++ Python-review-code.py
@@ -1,3 +1,17 @@
 def create_youtube_video(title, description):
-	youtube_vid = {"Title":title, "Description": description, "Likes": 0, "Dislikes": 0, "Comments": {"username": ""}}
+	youtube_vid = {"Title":title, "Description": description, "Likes": 0, "Dislikes": 0, "Comments": {}}
 	return youtube_vid
+
+def likes(video):
+	if "Likes" in video:
+		video["Likes"] += 1
+	return video
+
+def dislikes(video):
+	if "Dislikes" in video:
+		video["Dislikes"] += 1
+	return video
+
+def add_comment(video, username, commenttext):
+	video["Comments"][username] = commenttext
+	return video
